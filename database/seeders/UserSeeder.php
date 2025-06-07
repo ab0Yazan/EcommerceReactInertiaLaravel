4cd89ca7a697cc997ce6e8144b445f5e25628cee
ab0Yazan <muhamed41220@gmail.com>
2025-06-07 16:18:25 +0300

    prepare filament admin panel

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Enums\RolesEnum;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,19 +15,23 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $password = Hash::make('12345678');
         User::factory()->create([
             'name' => 'User',
-            'email' => 'user@example.com'
+            'email' => 'user@example.com',
+            'password' => $password,
         ])->assignRole(RolesEnum::User->value);
 
         User::factory()->create([
             'name' => 'Vendor',
             'email' => 'vendor@example.com',
+            'password' => $password,
         ])->assignRole(RolesEnum::Vendor->value);
 
         User::factory()->create([
             'name' => 'Admin',
-            'email' => 'admin@example.com'
+            'email' => 'admin@example.com',
+            'password' => $password,
         ])->assignRole(RolesEnum::Admin->value);
     }
 }
